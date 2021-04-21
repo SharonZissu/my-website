34375e14bc4a5d573c4784f0f1902fdc6f56e238
sharonzissu10 <sharonzissu10@gmail.com>
2021-04-21 16:46:14 +0300

    fix database string

--- helpers/db.js
+++ helpers/db.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 export async function connectDatabase() {
   const client = await MongoClient.connect(
-    `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@cluster0.im1am.mongodb.net/<dbname>?authSource=admin&replicaSet=atlas-5tp3w9-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true`
+    `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@cluster0.im1am.mongodb.net/personal-website?authSource=admin&replicaSet=atlas-5tp3w9-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true`
   );
   return client;
 }
